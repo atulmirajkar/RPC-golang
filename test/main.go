f0f5a92acc12291f2ec313f54a2e77f192792861
atulmirajkar <atulmirajkar@gmail.com>
2015-02-18 17:44:12 -0500

    add test files

---
+++ test/main.go
@@ -0,0 +1,48 @@
+package main
+
+import(
+	"rpcprotocol/rpcclient"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main(){
+
+	//unmarshal config file in configObject
+	configFilePath := os.Args[1]
+	configObject := &rpcclient.ConfigType{}
+	if err:=configObject.ReadConfig(configFilePath); err!=nil{
+		fmt.Println("Error reading Config file",err)
+		return
+	}
+	
+
+	//create the connection
+	//number of simultaneous channels should be taken from config file
+	numChannels := 10
+	network := configObject.Protocol
+	address := configObject.IpAddress + ":" + strconv.Itoa(configObject.Port)
+	client := &rpcclient.RPCClient{}
+	
+	//create new client
+	if err :=client.NewClient(network,address,numChannels); err!=nil{
+		fmt.Println(err)
+		return
+	
+	} 
+	
+	//make asychronous calls
+	if err := client.CreateAsyncRPC(os.Args[2:]); err!=nil{
+		fmt.Println(err)
+		return
+	}
+	
+	//process replies from server
+	if err := client.ProcessReplies(len(os.Args)-2); err!=nil{
+		fmt.Println(err)
+		return
+	}
+	
+	
+}
